6c9716c9bf17f814a068651d087e6d18096a46e9
Dennis Geldhof <dennis@geldhof.eu>
2025-05-04 13:42:20 +0200

    Clean up lambda

--- server-hosting/status/lambda.ts
+++ server-hosting/status/lambda.ts
@@ -9,49 +9,55 @@ exports.handler = async function (event: any) {
   const fieldName = event.info.fieldName;
 
   if (fieldName == 'start') {
-    console.log("Attempting to start game server", instanceId);
-
-    try {
-      const res = await client.send(commandStart);
-      const resStatus = await client.send(commandStatus)
-
-      console.log(JSON.stringify(res));
-      console.log(JSON.stringify(resStatus));
-
-      return {
-        status: res?.StartingInstances ? res?.StartingInstances[0]?.CurrentState?.Name ?? "??" : "??",
-        previousStatus: res?.StartingInstances ? res?.StartingInstances[0]?.PreviousState?.Name ?? "??" : "??",
-        detail: resStatus?.InstanceStatuses ? resStatus?.InstanceStatuses[0]?.InstanceStatus?.Details?.[0].Status ?? "??" : "??"
-      }
-    }
-    catch (err) {
-      console.log(JSON.stringify(err));
-      return {
-        status: "Failed to start satisfactory server " + JSON.stringify(err)
-      }
-    }
+    return await start();
   }
 
   if (fieldName == 'status') {
-    try {
-      const res = await client.send(commandStatus)
-      console.log(JSON.stringify(res));
-
-      return {
-        status: res?.InstanceStatuses ? res?.InstanceStatuses[0]?.InstanceState?.Name ?? "??" : "??",
-        detail: res?.InstanceStatuses ? res?.InstanceStatuses[0]?.InstanceStatus?.Details : "??",
-        previousStatus: JSON.stringify(res)
-      }
-    }
-    catch (err) {
-      console.log(JSON.stringify(err));
-      return {
-        status: "Failed to get status " + JSON.stringify(err)
-      }
-    };
+    return await status();
   }
 
   console.log(`oops, fieldName not found: ${fieldName}`);
-
   return { statusCode: 404 };
+}
+
+async function start() {
+  console.log("Attempting to start game server", instanceId);
+
+  try {
+    const res = await client.send(commandStart);
+    const resStatus = await client.send(commandStatus)
+
+    console.log(JSON.stringify(res));
+    console.log(JSON.stringify(resStatus));
+
+    return {
+      status: res?.StartingInstances ? res?.StartingInstances[0]?.CurrentState?.Name ?? "??" : "??",
+      previousStatus: res?.StartingInstances ? res?.StartingInstances[0]?.PreviousState?.Name ?? "??" : "??",
+      detail: resStatus?.InstanceStatuses ? resStatus?.InstanceStatuses[0]?.InstanceStatus?.Details?.[0].Status ?? "??" : "??"
+    }
+  }
+  catch (err) {
+    console.log(JSON.stringify(err));
+    return {
+      status: "Failed to start satisfactory server " + JSON.stringify(err)
+    }
+  }
+}
+
+async function status() {
+  try {
+    const res = await client.send(commandStatus)
+    console.log(JSON.stringify(res));
+
+    return {
+      status: res?.InstanceStatuses ? res?.InstanceStatuses[0]?.InstanceState?.Name ?? "??" : "??",
+      detail: res?.InstanceStatuses ? res?.InstanceStatuses[0]?.InstanceStatus?.Details?.[0]?.Status : "??",
+    }
+  }
+  catch (err) {
+    console.log(JSON.stringify(err));
+    return {
+      status: "Failed to get status " + JSON.stringify(err)
+    }
+  };
 }
