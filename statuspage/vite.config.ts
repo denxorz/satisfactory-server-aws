32a4ac300c964dd1df8083d3dff1737937ec4324
Dennis Geldhof <dennis@geldhof.eu>
2025-07-30 13:16:38 +0200

    Fix build

--- statuspage/vite.config.ts
+++ statuspage/vite.config.ts
@@ -1,6 +1,8 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vuetify from 'vite-plugin-vuetify'
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
 import eslint from 'vite-plugin-eslint'
 
 // https://vite.dev/config/
