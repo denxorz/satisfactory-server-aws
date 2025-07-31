ad2db560a2d29ad31080abc174cefe650867ab31
Dennis Geldhof <dennis@geldhof.eu>
2025-07-31 16:36:11 +0200

    Update eslint

---
+++ statuspage/eslint.config.js
@@ -0,0 +1,78 @@
+import js from '@eslint/js'
+import typescript from '@typescript-eslint/eslint-plugin'
+import typescriptParser from '@typescript-eslint/parser'
+import prettier from 'eslint-config-prettier'
+import vue from 'eslint-plugin-vue'
+import vueParser from 'vue-eslint-parser'
+
+export default [
+  {
+    ignores: [
+      'node_modules/',
+      'dist/',
+      'build/',
+      '*.min.js',
+      '*.min.css',
+      'graphql.schema.json',
+      'src/gql/',
+      '*.tsbuildinfo',
+      '.env*',
+    ],
+  },
+  js.configs.recommended,
+  {
+    files: ['**/*.{js,ts,vue}'],
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      globals: {
+        process: 'readonly',
+        window: 'readonly',
+        console: 'readonly',
+      },
+    },
+    plugins: {
+      '@typescript-eslint': typescript,
+      vue: vue,
+    },
+    rules: {
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        {
+          argsIgnorePattern: '^_',
+          varsIgnorePattern: '^_',
+          ignoreRestSiblings: true,
+        },
+      ],
+      '@typescript-eslint/no-explicit-any': 'warn',
+      '@typescript-eslint/no-non-null-assertion': 'warn',
+
+      'vue/multi-word-component-names': 'off',
+
+      'no-unused-vars': 'off', // Turn off base rule as it conflicts with TypeScript rule
+      'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    },
+  },
+  {
+    files: ['**/*.{js,ts}'],
+    languageOptions: {
+      parser: typescriptParser,
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+      },
+    },
+  },
+  {
+    files: ['**/*.vue'],
+    languageOptions: {
+      parser: vueParser,
+      parserOptions: {
+        parser: typescriptParser,
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+      },
+    },
+  },
+  prettier,
+]
--- statuspage/vite.config.ts
+++ statuspage/vite.config.ts
@@ -1,7 +1,6 @@
+import eslint from '@nabla/vite-plugin-eslint'
 import vue from '@vitejs/plugin-vue'
 import { defineConfig } from 'vite'
-// @ts-expect-error See https://github.com/gxmari007/vite-plugin-eslint/issues/79
-import eslint from 'vite-plugin-eslint'
 import vuetify from 'vite-plugin-vuetify'
 
 export default defineConfig({
